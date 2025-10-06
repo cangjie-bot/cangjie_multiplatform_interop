fbbbc90cbbbb86d9657f89670996d133f61325e8
Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>
2025-10-06 11:08:24 +0700

    ObjCInteropGen: avoid symbol redefinition when including the same header from different source files in one .toml

--- objc/src/ObjCInteropGen/InputFile.h
+++ objc/src/ObjCInteropGen/InputFile.h
@@ -8,12 +8,43 @@
 #ifndef INPUTFILE_H
 #define INPUTFILE_H
 
-#include "Symbol.h"
 #include <filesystem>
 #include <set>
 #include <vector>
 
+#ifdef OBJCINTEROPGEN_NO_WARNINGS
+#// [[nodiscard]] is ignored in C++17 when being applied to constructors.  Older
+#// GCC issues warnings on that.
+#if __has_cpp_attribute(nodiscard) > 201603L
+#define OBJCINTEROPGEN_NODISCARD_CONSTRUCTOR [[nodiscard]]
+#else
+#define OBJCINTEROPGEN_NODISCARD_CONSTRUCTOR
+#endif
+#endif
+
+class FileLevelSymbol;
 class InputDirectory;
+
+struct LineCol {
+    unsigned line_;
+    unsigned col_;
+
+    friend bool operator<(const LineCol& loc1, const LineCol& loc2) noexcept
+    {
+        return loc1.line_ < loc2.line_ || (loc1.line_ == loc2.line_ && loc1.col_ < loc2.col_);
+    }
+};
+
+struct Location : LineCol {
+    std::filesystem::path file_;
+
+    [[nodiscard]] auto is_null() const noexcept
+    {
+        return file_.empty();
+    }
+};
+
+static Location null_location = Location{{0, 0}, {}};
 
 class InputFile final {
     struct SymbolComparator {
@@ -22,6 +53,10 @@ class InputFile final {
 
     InputDirectory* directory_;
     std::filesystem::path path_;
+
+    std::set<LineCol> cursors_up_to_this_translation_;
+    std::set<LineCol> cursors_in_this_translation_;
+
     std::multiset<FileLevelSymbol*, SymbolComparator> symbols_;
 
     friend class FileLevelSymbol;
@@ -60,6 +95,10 @@ public:
     {
         return cend();
     }
+
+    void next_translation();
+
+    [[nodiscard]] bool add_cursor(const LineCol& location);
 };
 
 class InputDirectory final {
@@ -102,10 +141,13 @@ public:
 class Inputs final {
     std::vector<InputDirectory*> directories_;
 
+    std::set<std::string> builtin_cursors_up_to_this_translation_;
+    std::set<std::string> builtin_cursors_in_this_translation_;
+
 public:
     Inputs() = default;
 
-    InputFile* operator[](const std::filesystem::path& path);
+    InputFile& operator[](const std::filesystem::path& path);
 
     auto cbegin() const
     {
@@ -126,6 +168,10 @@ public:
     {
         return cend();
     }
+
+    void next_translation();
+
+    [[nodiscard]] bool add_cursor(const Location& location, const std::string& name);
 };
 
 extern Inputs inputs;
