41c7b5d4446b098318ccedb4668a0842bc7ef243
George Timoshenko <george.timoshenko@huawei.com>
2025-10-06 18:27:00 +0700

    test(refgluecode): 6-interface - Objc class implementing a CJ interface
    Signed-off-by: gtimoshenko <george.timoshenko@huawei.com>

---
+++ objc/test/RefGlueCode/objc-uses-cj/6-interface/generated/Animal.m
@@ -0,0 +1,55 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+// This source file is part of the Cangjie project, licensed under Apache-2.0
+// with Runtime Library Exception.
+//
+// See https://cangjie-lang.cn/pages/LICENSE for license information.
+
+#import <dlfcn.h>
+#import "Cangjie.h"
+#import "Animal.h"
+
+extern void CJImpl_ObjC_cjworld_AnimalImpl_SayImpl(void*);
+extern void CJImpl_ObjC_cjworld_AnimalImpl_EatImpl(void*);
+extern void CJImpl_ObjC_cjworld_AnimalImpl_staticTest(void*);
+
+static void* CJWorldDLHandle = NULL;
+static struct RuntimeParam defaultCJRuntimeParams = {0};
+
+// the default implementation of Animal interface
+//@CJMirror
+@implementation AnimalImpl
+
++ (void)initialize {
+    if (self == [AnimalImpl class]) {
+        defaultCJRuntimeParams.logParam.logLevel = RTLOG_ERROR;
+        if (InitCJRuntime(&defaultCJRuntimeParams) != E_OK) {
+            NSLog(@"ERROR: Failed to initialize Cangjie runtime");
+            exit(1);
+        }
+
+        if (LoadCJLibraryWithInit("libcjworld.dylib")) {
+            NSLog(@"ERROR: Failed to init cjworld library");
+            exit(1);
+        }
+
+        if ((CJWorldDLHandle = dlopen("libcjworld.dylib", RTLD_LAZY)) == NULL) {
+            NSLog(@"ERROR: Failed to open cjworld library");
+            NSLog(@"%s", dlerror());
+            exit(1);
+        }
+    }
+}
+
+- (void)Say {
+    CJImpl_ObjC_cjworld_AnimalImpl_SayImpl((__bridge void*)self);
+}
+
+- (void)Eat {
+    CJImpl_ObjC_cjworld_AnimalImpl_EatImpl((__bridge void*)self);
+}
+
++ (void)staticTest:(id<Animal>) animal {
+    CJImpl_ObjC_cjworld_AnimalImpl_staticTest((__bridge void*)animal);
+}
+
+@end
