d4a8472957e71ea94f315f966c223d59fdb8b99d
Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>
2025-10-06 11:08:24 +0700

    feat: avoid symbol redefinition when including the same header in different files
    Signed-off-by: Mikhail Nesterov <nesterov.mikhail2@huawei-partners.com>

--- objc/src/ObjCInteropGen/InputFile.h
+++ objc/src/ObjCInteropGen/InputFile.h
@@ -8,12 +8,56 @@
 #ifndef INPUTFILE_H
 #define INPUTFILE_H
 
-#include "Symbol.h"
+#include <climits>
 #include <filesystem>
 #include <set>
+#include <unordered_set>
 #include <vector>
 
+#ifdef OBJCINTEROPGEN_NO_WARNINGS
+#// [[nodiscard]] is ignored in C++17 when being applied to constructors.  Older
+#// GCC issues warnings on that.
+#if __has_cpp_attribute(nodiscard) > 201603L
+#define OBJCINTEROPGEN_NODISCARD_CONSTRUCTOR [[nodiscard]]
+#else
+#define OBJCINTEROPGEN_NODISCARD_CONSTRUCTOR
+#endif
+#endif
+
+class FileLevelSymbol;
 class InputDirectory;
+
+struct LineCol {
+    unsigned line_;
+    unsigned col_;
+
+    friend bool operator<(const LineCol& loc1, const LineCol& loc2) noexcept
+    {
+        return loc1.line_ < loc2.line_ || (loc1.line_ == loc2.line_ && loc1.col_ < loc2.col_);
+    }
+
+    friend bool operator==(const LineCol& loc1, const LineCol& loc2) noexcept
+    {
+        return loc1.line_ == loc2.line_ && loc1.col_ == loc2.col_;
+    }
+};
+
+template <> struct std::hash<LineCol> {
+    size_t operator()(const LineCol& pos) const noexcept
+    {
+        constexpr auto half_width = sizeof(size_t) * CHAR_BIT / 2;
+        return (static_cast<size_t>(pos.line_) << half_width) + pos.line_;
+    }
+};
+
+struct Location : LineCol {
+    std::filesystem::path file_;
+
+    [[nodiscard]] bool is_null() const noexcept
+    {
+        return file_.empty();
+    }
+};
 
 class InputFile final {
     struct SymbolComparator {
@@ -22,6 +66,10 @@ class InputFile final {
 
     InputDirectory* directory_;
     std::filesystem::path path_;
+
+    std::unordered_set<LineCol> cursors_up_to_this_translation_;
+    std::unordered_set<LineCol> cursors_in_this_translation_;
+
     std::multiset<FileLevelSymbol*, SymbolComparator> symbols_;
 
     friend class FileLevelSymbol;
@@ -60,6 +108,10 @@ public:
     {
         return cend();
     }
+
+    void next_translation();
+
+    [[nodiscard]] bool add_cursor(const LineCol& location);
 };
 
 class InputDirectory final {
@@ -102,10 +154,13 @@ public:
 class Inputs final {
     std::vector<InputDirectory*> directories_;
 
+    std::set<std::string> builtin_cursors_up_to_this_translation_;
+    std::set<std::string> builtin_cursors_in_this_translation_;
+
 public:
     Inputs() = default;
 
-    InputFile* operator[](const std::filesystem::path& path);
+    InputFile& operator[](const std::filesystem::path& path);
 
     auto cbegin() const
     {
@@ -126,6 +181,10 @@ public:
     {
         return cend();
     }
+
+    void next_translation();
+
+    [[nodiscard]] bool add_cursor(const Location& location, const std::string& name);
 };
 
 extern Inputs inputs;
