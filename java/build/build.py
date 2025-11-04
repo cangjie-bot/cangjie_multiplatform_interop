07c685ac8d21bb43dcbd28486573926d2568c5df
Mark-chen <chenxuesheng1@huawei.com>
2025-11-04 09:55:42 +0800

    [interop] fix jdk thirdparty

--- java/build/build.py
+++ java/build/build.py
@@ -229,15 +229,19 @@ def fetch_jdk(target_dir):
 
         LOG.info(f"Delete temporary clone directory: {clone_dir}")
         shutil.rmtree(clone_dir)
+
+
         # Apply Jdk_interop specific modification
         """Apply a patch file to the target directory"""
         patch_path = os.path.join(JAVA_INTEROP_THIRD_PARTY, "jdk_interop.patch")
+
         if os.path.exists(patch_path):
             patch_cmd = f"patch -p1 -l -f < {patch_path}"
-            patch_process = subprocess.Popen(patch_cmd, shell=True, stdout=PIPE, cwd=HOME_DIR)
+            LOG.info("CMDPATH: %s", HOME_DIR)
+            patch_process = subprocess.run(patch_cmd, shell=True, stdout=PIPE, cwd=HOME_DIR, check=True)
             LOG.info('Patch applied successfully')
         else:
-            LOG.info('Warning: jdk_interop.patch not found at {HOME_DIR}')
+            LOG.info("Warning: jdk_interop.patch not found at %s", HOME_DIR)
     else:
         LOG.info('jdk directory already exists, skipping fetch\n')
 
@@ -352,6 +356,8 @@ def clean(args):
         cwd=MIRROR_GEN_DIR,
         stdout=PIPE,
     )
+    jdk_dir = os.path.join(JAVA_INTEROP_THIRD_PARTY, "jdk")
+    subprocess.run(f"rm -rf {jdk_dir}", shell=True, check=True)
     log_output(output)
     LOG.info("end clean java-binding-gen\n")
     LOG.info("begin clean interoplib...\n")
