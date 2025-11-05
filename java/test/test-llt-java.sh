f14e2becf8662e7b5bc29f0c9cdd7d27598b52ca
gtimoshenko <george.timoshenko@huawei.com>
2025-11-05 15:04:41 +0700

    fix(test): fixup script test-llt-java
    Signed-off-by: gtimoshenko <george.timoshenko@huawei.com>

--- java/test/test-llt-java.sh
+++ java/test/test-llt-java.sh
@@ -1,21 +1,20 @@
+#!/bin/sh
 # Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
 # This source file is part of the Cangjie project, licensed under Apache-2.0
 # with Runtime Library Exception.
 #
 # See https://cangjie-lang.cn/pages/LICENSE for license information.
 
-#!/bin/sh
-
 BASE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-BUILD=${BASE}/test/LLT/build
+BUILD=${BASE}/LLT/build
 TMP=${BUILD}/test_tmp
 mkdir -p ${TMP}
 cd ${BUILD}
 
-# TODO check ${BASE}/test/HLT with framework/main.py inside is available
+# TODO check ${BASE}/HLT with framework/main.py inside is available
 
-python3 ${BASE}/test/HLT/framework/main.py --fail_exit -pFAIL -pPASS --fail-verbose --debug ../ \
+python3 ${BASE}/HLT/framework/main.py --fail_exit -pFAIL -pPASS --fail-verbose --debug ../ \
 --temp_dir=${TMP} --json_output=${TMP}/run_result.json -j7 \
 --test_cfg=../interop_test.cfg --test_list=../testlist_java
 
