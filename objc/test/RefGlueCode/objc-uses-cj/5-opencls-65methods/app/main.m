01c7159ca095adddc9936f286c619e64f3a8896b
gtimoshenko <george.timoshenko@huawei.com>
2025-11-13 20:03:02 +0700

    test(refgluecode): 5-opencls-65methods - subcase: 65 methods do not fit in mask

---
+++ objc/test/RefGlueCode/objc-uses-cj/5-opencls-65methods/app/main.m
@@ -0,0 +1,54 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+// This source file is part of the Cangjie project, licensed under Apache-2.0
+// with Runtime Library Exception.
+//
+// See https://cangjie-lang.cn/pages/LICENSE for license information.
+
+#import <Foundation/Foundation.h>
+#import "A.h"
+#import "AA.h"
+
+extern void CJImpl_ObjC_cjworld_cjGC();
+
+void TestFoo() {
+ // @autoreleasepool { // uncomment to try with ARCOFF=true
+    printf("\nObjC: TestFoo STARTED\n");
+
+    A* aa = [[AA alloc] init];
+
+    printf("\nObjC: TestFoo execute aa.foo0:\n");
+    [aa foo0];
+
+    printf("\nObjC: TestFoo execute aa.foo42:\n");
+    [aa foo42];
+
+    printf("\nObjC: TestFoo execute aa.foo62 (not overridden):\n");
+    [aa foo62];
+
+    printf("\nObjC: TestFoo execute aa.foo63:\n");
+    [aa foo63];
+
+    printf("\nObjC: TestFoo execute aa.foo64:\n");
+    [aa foo64];
+
+    printf("\nObjC: TestFoo execute aa.foo65:\n");
+    [aa foo65];
+
+    printf("\nObjC: TestFoo execute aa.foo1 to test aa from CJ side:\n");
+    [aa foo1];
+
+    printf("\nObjC: TestFoo is finished, after return - the autoreleasepool (by ARC) is closed so 1 TransitionII ia expected for aa:\n");
+// [aa autorelease]; // uncomment to try with ARCOFF=true
+// }
+}
+
+int main(int argc, char** argv) {
+
+    TestFoo();
+
+    printf("\nObjC: after TestFoo call cjGC, 1 TransitionIV is expected:\n");
+    CJImpl_ObjC_cjworld_cjGC();
+    printf("ObjC: cjGC COMPLETED\n");
+
+    return 0;
+}
