528bd2bcc8ff1ac9177b45295a86aa9ae92bb724
gtimoshenko <george.timoshenko@huawei.com>
2025-11-18 19:41:44 +0700

    feat(interoplib): drop dlopen in libinteroplib.objclib.dylib
    Signed-off-by: gtimoshenko <george.timoshenko@huawei.com>

--- objc/src/interoplib/src/objclib/cjinterop.h
+++ objc/src/interoplib/src/objclib/cjinterop.h
@@ -5,8 +5,15 @@
 // See https://cangjie-lang.cn/pages/LICENSE for license information.
 
 #import <Foundation/Foundation.h>
-#import <stddef.h>
 
+/**
+ * Initialize Cangjie runtime and call LoadCJLibraryWithInit for `cj_gluecode_lib_name` library.
+ */
 bool initCJRuntime(const char* cj_gluecode_lib_name);
 
+/**
+ * Calucate override mask for methods in `methods` array of `baseCls` class that may be overridden in `selfCls` class.
+ * The length of `methods` array is passed as `len`. If it is greater than 64 only first 64 methods participate in the mask.
+ * The i-th bit of the calculated mask is set to 1, when the i-th method is overridden in `selfCls`.
+ */
 uint64_t calcOverrideMask(Class baseCls, Class selfCls, SEL* methods, int len);
