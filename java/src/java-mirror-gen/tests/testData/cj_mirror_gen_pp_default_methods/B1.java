73987dcb55faa5ed5e92a7fcf81432be960665f6
Daria Knysh <knysh.daria@h-partners.com>
2026-01-13 19:11:33 +0700

    feat(javainteropgen): generate public & protected API for package-private types
    Signed-off-by: Daria Knysh <knysh.daria@h-partners.com>

---
+++ java/src/java-mirror-gen/tests/testData/cj_mirror_gen_pp_default_methods/B1.java
@@ -0,0 +1,11 @@
+// Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+// This source file is part of the Cangjie project, licensed under Apache-2.0
+// with Runtime Library Exception.
+//
+// See https://cangjie-lang.cn/pages/LICENSE for license information.
+
+import package1.A1;
+import package1.A2;
+
+public class B1 implements A1, A2 {
+}
