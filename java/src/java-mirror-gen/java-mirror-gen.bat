455d3bc4b77abf2b979e76583f7f54fbf106e946
Daria Knysh <knysh.daria@h-partners.com>
2026-01-23 14:30:16 +0700

    fix(javainteropgen): add proper help option processing for java-mirror-gen in launch scripts
    Signed-off-by: Daria Knysh <knysh.daria@h-partners.com>

--- java/src/java-mirror-gen/java-mirror-gen.bat
+++ java/src/java-mirror-gen/java-mirror-gen.bat
@@ -124,17 +124,17 @@ IF NOT "%1"=="" (
     IF "%1"=="--help" (
         CALL :setHelp
         SHIFT
-        GOTO :generatorRun
+        GOTO :eof
     )
     IF "%1"=="-?" (
         CALL :setHelp
         SHIFT
-        GOTO :generatorRun
+        GOTO :eof
     )
     IF "%1"=="-h" (
         CALL :setHelp
         SHIFT
-        GOTO :generatorRun
+        GOTO :eof
     )
     IF "%1"=="--verbose" (
         CALL :setVerbose
@@ -228,9 +228,55 @@ EXIT /B 0
 EXIT /B 0
 
 :setHelp
-    SET jvmGeneratorArgs=
-    SET generatorArgs=--help
-    SET inputClasses=
+    ECHO Usage: Java mirror generator [options] [type-names]
+    ECHO The Java mirror generator can be run in two ways:
+    ECHO   sh ./java-mirror-gen.sh [options] [type-names]
+    ECHO     or
+    ECHO   sh ./java-mirror-gen.sh [options] -jar jar-file (single-jar mode)
+    ECHO     where
+    ECHO   [type-names] are the fully qualified names of Java classes and interfaces
+    ECHO   [jar-file] is the pathname of a single jar file
+    ECHO   possible [options] include:
+    ECHO     --android-jar pathname, -a pathname    (mandatory when Android API is used)
+    ECHO       pathname must point to the android.jar file from the Android SDK.
+    ECHO(
+    ECHO     --destination directory, -d directory
+    ECHO       Specify output directory for generated mirrors files.
+    ECHO       If this option is not specified, the current directory is assumed.
+    ECHO(
+    ECHO     --class-path path, -cp path
+    ECHO       path is list of pathname of directories, .jar files and .zip files, separated with semicolons : to
+    ECHO       containing input .class files or .jar file dependencies.
+    ECHO(
+    ECHO     --package-name name, -p name
+    ECHO       name is the name of the Cangjie package into which all generated mirror types will be placed.
+    ECHO       If this option is not specified, package name will be UNNAMED.
+    ECHO(
+    ECHO     --closure-depth-limit number, -c number
+    ECHO       number is a non-negative decimal integer limiting the depth of dependency graph
+    ECHO       scanning when determining the set of types and their members that will be mirrored.
+    ECHO(
+    ECHO     -jar jar-file
+    ECHO       jar-file is the pathname of the jar file to be processed in the "single-jar" mode
+    ECHO(
+    ECHO     --package-list pathname, -l pathname
+    ECHO       pathname is the pathname of a plain text file containing a list of package names.
+    ECHO       Only the class files that belong to the listed packages and their dependencies will be mirrored.
+    ECHO       Requires -jar.
+    ECHO(
+    ECHO     --imports pathname, --import-mappings pathname, -i pathname
+    ECHO       pathname is the pathname of a plain text file containing mirror type mappings
+    ECHO       accumulated during previous mirror generator runs.
+    ECHO       Requires -l and -jar.
+    ECHO(
+    ECHO     --help, -h, -?
+    ECHO       Prints a help message with a brief description of the command-line syntax and all options and exits.
+    ECHO(
+    ECHO     --verbose, -v
+    ECHO       Instructs the mirror generator to emit messages about what it is doing.
+    ECHO(
+    ECHO     For example, to generate mirrors types for java.util.HashMap run:
+    ECHO       .\java-mirror-gen.bat -d hmpackage -p hashmap java.util.HashMap
 EXIT /B 0
 
 :setVerbose
