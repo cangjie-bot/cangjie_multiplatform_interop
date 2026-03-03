b0bebc954811e484ec08e80f275b8f185f6039da
Andrew Boyarshin <boyarshin.andrew@huawei.com>
2026-03-03 19:35:02 +0700

    feat(objcinteropgen): replace toml++ by tinytoml
    Signed-off-by: Andrew Boyarshin <boyarshin.andrew@huawei.com>

--- objc/src/ObjCInteropGen/PackageOutputs.cpp
+++ objc/src/ObjCInteropGen/PackageOutputs.cpp
@@ -17,25 +17,23 @@ namespace objcgen {
 
 static std::string compute_output_root(const std::string_view package_name)
 {
-    if (auto* output_roots_any = config.get("output-roots")) {
-        if (const auto* output_roots_table = output_roots_any->as_table()) {
-            if (output_roots_table->empty()) {
-                fatal("`packages` entry ", package_name,
-                    " has no `output-path` specified and there are no `output-roots`");
-            }
-
-            if (output_roots_table->size() != 1) {
-                fatal("`packages` entry ", package_name,
-                    " has no `output-path` or `output-root` specified and there are multiple `output-roots`");
-            }
-
-            const auto it = output_roots_table->cbegin();
-            assert(it != output_roots_table->cend());
-            const auto output_root_name = it->first;
-            return std::string(output_root_name.str());
+    if (const auto* output_roots_any = config.find("output-roots")) {
+        if (!output_roots_any->is<toml::Table>()) {
+            fatal("`output-roots` should be a TOML table");
+        }
+        const auto& output_roots_table = output_roots_any->as<toml::Table>();
+        if (output_roots_table.empty()) {
+            fatal("`packages` entry ", package_name, " has no `output-path` specified and there are no `output-roots`");
         }
 
-        fatal("`output-roots` should be a TOML table");
+        if (output_roots_table.size() != 1) {
+            fatal("`packages` entry ", package_name,
+                " has no `output-path` or `output-root` specified and there are multiple `output-roots`");
+        }
+
+        const auto it = output_roots_table.cbegin();
+        assert(it != output_roots_table.cend());
+        return it->first;
     }
 
     fatal("`packages` entry ", package_name, " has no `output-path` specified and there are no `output-roots`");
@@ -78,42 +76,39 @@ static std::string compute_output_path_by_root_path(const std::string_view packa
 static std::string compute_output_path_by_root_name(const std::string_view package_name,
     const std::string_view output_root_name, const std::string_view package_cangjie_name)
 {
-    if (auto* output_roots_any = config.get("output-roots")) {
-        if (auto* output_roots_table = output_roots_any->as_table()) {
-            for (auto&& [output_root_key, output_root_any] : *output_roots_table) {
-                if (output_root_key != output_root_name) {
-                    continue;
-                }
-
-                if (const auto* output_root_table = output_root_any.as_table()) {
-                    const auto output_path = get_string_value(
-                        *output_root_table, package_name, "path", [&output_root_name](auto&) -> std::string {
-                            fatal("`output-roots` entry `", output_root_name, "` should define a `path` property");
-                        });
-
-                    return compute_output_path_by_root_path(
-                        package_name, output_root_name, output_path, package_cangjie_name);
-                }
-
-                fatal("`output-roots` entry `", output_root_name, "` should be a TOML table");
+    if (const auto* output_roots_any = config.find("output-roots")) {
+        if (!output_roots_any->is<toml::Table>()) {
+            fatal("`output-roots` should be a TOML table");
+        }
+        for (auto&& [output_root_key, output_root_any] : output_roots_any->as<toml::Table>()) {
+            if (output_root_key != output_root_name) {
+                continue;
             }
 
-            fatal("`packages` entry ", package_name, " has no `output-path` specified and `output-root` `",
-                output_root_name, "` was not found");
+            if (!output_root_any.is<toml::Table>()) {
+                fatal("`output-roots` entry `", output_root_name, "` should be a TOML table");
+            }
+            const auto output_path = get_string_value(output_root_any.as<toml::Table>(), package_name, "path",
+                [&output_root_name](const auto&) -> std::string {
+                    fatal("`output-roots` entry `", output_root_name, "` should define a `path` property");
+                });
+
+            return compute_output_path_by_root_path(package_name, output_root_name, output_path, package_cangjie_name);
         }
 
-        fatal("`output-roots` should be a TOML table");
+        fatal("`packages` entry ", package_name, " has no `output-path` specified and `output-root` `",
+            output_root_name, "` was not found");
     }
 
     fatal("`packages` entry ", package_name, " has no `output-path` specified and there are no `output-roots`");
 }
 
 std::string compute_output_path(
-    const std::string& name, const toml::table& config, const std::string_view package_cangjie_name)
+    const std::string& name, const toml::Table& config, const std::string_view package_cangjie_name)
 {
-    return get_string_value(config, name, "output-path", [&name, &package_cangjie_name](const toml::table& config) {
-        const auto output_root = get_string_value(
-            config, name, "output-root", [&name](const toml::table&) { return compute_output_root(name); });
+    return get_string_value(config, name, "output-path", [&name, &package_cangjie_name](const auto& config) {
+        const auto output_root =
+            get_string_value(config, name, "output-root", [&name](const auto&) { return compute_output_root(name); });
 
         return compute_output_path_by_root_name(name, output_root, package_cangjie_name);
     });
